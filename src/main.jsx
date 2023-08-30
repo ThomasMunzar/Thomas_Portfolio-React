31a01c19996e0ef3211d69c057e918485efc10c4
Thomas Munzar <tmunzar92@gmail.com>
2023-08-30 15:28:18 -0700

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//import pages
+import AboutMe from './components/pages/AboutMe.jsx';
+import Contact from './components/pages/Contact.jsx';
+import Portfolio from './components/pages/Portfolio.jsx';
+import Resume from './components/pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Portfolio />
+      },
+      {
+        path: '/about',
+        element: <AboutMe />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router={router} />
 )
