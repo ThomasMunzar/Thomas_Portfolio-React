16bff14dd8133e3b4f362606b671ce0118d1787a
Thomas Munzar <tmunzar92@gmail.com>
2023-09-01 16:54:17 -0700

    fixed bug, had infinite loop with NavBar looking for <NavBar/>"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import Header from './components/Header'
 
 function App() {
  return(
  <>
-<NavBar/>
+<Header/>
  <Outlet/>
  </>
  )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,31 @@
- function NavBar () {
-    return(
-    <h1>Nav Bar</h1>
-    )
- }
+import { Link } from 'react-router-dom';
 
- export default NavBar 
+
+
+
+function NavBar() {
+   return (
+      
+      <dev>
+        
+            <Link  className="nav-link text-light" to="/about">
+               About Me
+            </Link>
+
+             <Link  className="nav-link text-light" to="/portfolio">
+             Portfolio
+          </Link>
+           <Link  className="nav-link text-light" to="/contact">
+          Contact
+        </Link>
+        <Link  className="nav-link text-light" to="/resume">
+           Resume
+        </Link>
+        
+        
+        </dev>
+   );
+}
+
+
+export default NavBar 
