d0f0e118df426230738a493a2ea8950ebea20754
Thomas Munzar <tmunzar92@gmail.com>
2023-09-14 14:52:51 -0700

    added images now styling a little

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,61 +1,66 @@
 import React from "react";
-//Importing my project component
 import Project from "../project";
+import Forumz from '/assets/images/Forumz.png'
+import Diesel from '/assets/images/Diesel.png'
+import Jate from '/assets/images/JATE.png'
+import Password from '/assets/images/Password.png'
+import Whack from '/assets/images/Whack.png'
+import '../Project.css'
 
 
 export default function Portfolio() {
   const projects = [
     {
-     title: 'forumZ',
-     description: 'The all encompassing Reddit-like platform where you can explore different forumZ, make a post with an embedded video or picture, comment on others posts and even reply to comments!',
-     deployedLink: 'https://forumz-090c0b5b4bff.herokuapp.com/',
-     githubLink: 'https://github.com/jacob-kachmarek/forumZ'
-   },
+      title: 'forumZ',
+      description: 'The all encompassing Reddit-like platform where you can explore different forumZ, make a post with an embedded video or picture, comment on others posts and even reply to comments!',
+      deployedLink: 'https://forumz-090c0b5b4bff.herokuapp.com/',
+      githubLink: 'https://github.com/jacob-kachmarek/forumZ',
+      image: Forumz
+    },
+
     {
-      title: 'Wack',
+      title: 'Whack',
       description: 'A community driven project platform. Sign-up and create a project with a todo list. Get input from other users and comment on other users projects.',
       deployedLink: 'https://blooming-beyond-30251-89ceb9e97d3f.herokuapp.com/',
       githubLink: 'https://github.com/ThomasMunzar/Wack',
+      image: Whack
     },
-     {
+    {
       title: 'Alternate Fuel Locator',
       description: 'Enter your zipcode and find a gas station that offers alt. fuel!',
       deployedLink: 'https://iquiroz95.github.io/diesel-din-dance/',
       githubLink: 'https://github.com/iquiroz95/diesel-din-dance',
+      image: Diesel
     },
-     {
-      title: 'Full-Stack ToDo List',
-      description: 'Refactored code to make this a fully functional app',
-      deployedLink: 'https://to-do-list-express-9a349a9545e8.herokuapp.com/',
-      githubLink: 'https://github.com/ThomasMunzar/To-Do-List-Express',
-    },
-     {
+    {
       title: 'Password Generator',
       description: 'This simple app allows you generator a secure password. ',
       deployedLink: 'https://thomasmunzar.github.io/Best-Password-Generator-TM-/',
       githubLink: 'https://github.com/ThomasMunzar/Best-Password-Generator-TM-',
+      image: Password
     },
-     {
+    {
       title: 'Text Editor 3000',
       description: 'My first attempt at a PWA, a simple text editor. Downloadable and fucntional.',
       deployedLink: 'https://text-editor-3000-6b4fcab221b1.herokuapp.com/',
       githubLink: 'https://github.com/ThomasMunzar/Text-Editor-PWA',
+      image: Jate
     },
   ];
 
 
-    return (
+  return (
+    <div className="container project-container">
       <div className="container">
         <h1 className="d-flex justify-content-center">
           My Work
         </h1>
         <div>
-        {projects.map((project,index) =>(
-          <Project key={index} {...project} />
+          {projects.map((project, index) => (
+            <Project key={index} {...project} />
           ))}
         </div>
-
-       
       </div>
-    );
-  }
+    </div>
+  );
+}
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import './Project.css'
 
-function Project ({title, description, deployedLink, githubLink, image}) {
-    return(
-        <div className="card">
-            <img src={image} className="card-img-top" alt="{title}"/>
-            <div className="card-body">
-                <h5 className="card-title text-center">{title}</h5>
-                <p className="card-text text-center">{description}</p>
-                <a href={deployedLink} className="d-flex justify-content-center" >
-                    Deployed App
-                </a>
-                <br></br>
-                <a href={githubLink} className="d-flex justify-content-center">
-                    GitHub Repository
-                </a>
+function Project({ title, description, deployedLink, githubLink, image }) {
+    return (
+        <div className="project-card">
+
+            <div className="card">
+                <img src={image} className="card-img-top" alt="{title}" />
+                <div className="card-body">
+                    <h5 className="card-title text-center">{title}</h5>
+                    <p className="card-text text-center">{description}</p>
+                    <a href={deployedLink} className="d-flex justify-content-center" >
+                        Deployed App
+                    </a>
+                    <br></br>
+                    <a href={githubLink} className="d-flex justify-content-center">
+                        GitHub Repository
+                    </a>
+
+                </div>
 
             </div>
-
         </div>
     )
 }
