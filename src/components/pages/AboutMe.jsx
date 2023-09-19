192fb467d0102d8c41699985d00d077b1c9202fb
Thomas Munzar <tmunzar92@gmail.com>
2023-09-19 09:55:00 -0700

    edited Bio

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -14,11 +14,13 @@ export default function About() {
         <div>
           <img src={Thomas} alt="Image of a Web Developer" className="about-image"></img>
         </div>
-        <div className="text-center">
+        <div className="container text-center">
         <p className="">
-          Hello! My name is Thomas Munzar, I am from Oakland California and am excited to start my career in web development.  I studied environmental science at SF State and then fell in love with cooking.
-          I have working in many kitchens in the Bay Area but am now making a career change to web development. I studied web development at UC Berkeley Extention Coding Bootcamp. It was extremely challenging 
-          but I enjoyed the entire process but a very fun a rewarding journey, I am excited to learn so much more as I start my journey. Feel free to checkout some of the projects I have worked on!
+        Hello! I'm Thomas Munzar, originally from Oakland, California. My journey into web development took a unique path. After graduating from SF State with a degree in environmental science, I discovered a passion for cooking and spent time honing my culinary skills in various kitchens across the Bay Area. While I cherished those experiences, my curiosity for technology and coding led me to pivot towards a career in web development.
+I embarked on this exciting transition by enrolling in the UC Berkeley Extension Coding Bootcamp, where I immersed myself in the world of full-stack development, with a focus on the MERN stack. The bootcamp challenged me in countless ways, and I enjoyed every moment of the learning process. Through the program, I've acquired a robust set of skills in front-end and back-end development, database management, and creating dynamic web applications.
+As I embark on this new journey, I'm enthusiastic about continuing to learn, grow, and contribute to the dynamic field of web development. Please explore my portfolio to see some of the projects I've worked on, showcasing the skills I've acquired during my coding bootcamp.
+Thank you for visiting, and I look forward to connecting with fellow web enthusiasts and potential employers to create exciting digital experiences together!
+
         </p>
         </div>
 
